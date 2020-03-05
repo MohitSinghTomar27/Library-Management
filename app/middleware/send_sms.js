9ae37bc67c189e02d9a610e61276c8efa466f413
MohitSinghTomar27 <mtmohit59@gmail.com>
2020-03-05 11:50:29 +0530

    changes

--- app/middleware/send_sms.js
+++ app/middleware/send_sms.js
@@ -2,9 +2,9 @@ const Transaction=require('../model/transaction')
 const sendSms=function(req,res,next){
     const to_number=`+91${req.body.number}`
     console.log(to_number)
-    const accountSid = 'AC4e3becc5067c2e89df6a06f9e98ce0df';
-    const authToken = 'f33ea33287048637620bbd91d6d11220';
-    const client = require('twilio')(accountSid, authToken);
+    const accountMoh = 'process.env';
+    const authToken = 'preess.env';
+    const client = require('twilio')(accountMoh, authToken);
 
     client.messages
     .create({
